1ca303937bb65778e7d3fe306fdb7c867d2db5d6
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-12-26 02:00:52 +0100

    upgrade to firenze v0.4

--- dist/firenze-behavior-slug.js
+++ dist/firenze-behavior-slug.js
@@ -55,13 +55,13 @@ this["firenze"] = this["firenze"] || {}; this["firenze"]["SlugBehavior"] =
 
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	var _lodash = __webpack_require__(1);
 
@@ -110,7 +110,7 @@ this["firenze"] = this["firenze"] || {}; this["firenze"]["SlugBehavior"] =
 	//
 	// // create your Database instance...
 	//
-	// db.createCollectionClass({
+	// db.createCollection({
 	//   behaviors: [
 	//     SlugBehavior
 	//   ]
@@ -120,7 +120,7 @@ this["firenze"] = this["firenze"] || {}; this["firenze"]["SlugBehavior"] =
 	// If you want to pass extra configuration options:
 	//
 	// ```js
-	// db.createCollectionClass({
+	// db.createCollection({
 	//   behaviors: [
 	//     {
 	//       'class': SlugBehavior
@@ -160,6 +160,8 @@ this["firenze"] = this["firenze"] || {}; this["firenze"]["SlugBehavior"] =
 	//
 
 	var Slug = (function (_Behavior) {
+	  _inherits(Slug, _Behavior);
+
 	  function Slug() {
 	    _classCallCheck(this, Slug);
 
@@ -175,8 +177,6 @@ this["firenze"] = this["firenze"] || {}; this["firenze"]["SlugBehavior"] =
 	      separator: '-'
 	    }, this.options);
 	  }
-
-	  _inherits(Slug, _Behavior);
 
 	  _createClass(Slug, [{
 	    key: 'beforeSave',
